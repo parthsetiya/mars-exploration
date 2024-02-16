928b0df678e6f733190037925adbbc454a1be94e
Itsparrotdude <noah.volante@gmail.com>
2024-02-17 10:47:31 +1300

    Added player movement code

--- script/player.gd
+++ script/player.gd
@@ -1,11 +1,20 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@onready var animations = $AnimationPlayer
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
+func updateAnimation():
+	var direction = "Down"
+	if velocity.x < 0: direction = "Left"
+	elif velocity.x > 0: direction = "Right"
+	elif velocity.y < 0: direction = "Up"
+	
+	animations.play("walk" + direction)
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+	updateAnimation()
