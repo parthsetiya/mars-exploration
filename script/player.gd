165f8d2bc81b15f2e3151febd52544e2f6bb9a2d
Itsparrotdude <noah.volante@gmail.com>
2024-02-17 17:19:05 +1300

    Added walking left animation

--- script/player.gd
+++ script/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 200
 @onready var animations = $AnimationPlayer
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
