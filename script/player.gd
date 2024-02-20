883e7a44af2f3142a8bdc18b558df66177ceb2ab
Itsparrotdude <noah.volante@gmail.com>
2024-02-21 10:38:34 +1300

    Organised files working on animation tree

--- script/player.gd
+++ script/player.gd
@@ -7,13 +7,16 @@ func get_input():
 	velocity = input_direction * speed
 
 func updateAnimation():
-	if velocity.length() == 0:
+	$AnimationTree.set("parameters/Idle/blend_position", velocity)
+	if velocity.length() == 0: 
 		animations.stop()
+		
 	else: 
 		var direction = "Down"
 		if velocity.x < 0: direction = "Left"
 		elif velocity.x > 0: direction = "Right"
 		elif velocity.y < 0: direction = "Up"
+		#elif velocity.x < 0: direction = "Idle" 
 
 		animations.play("walk" + direction)
 
