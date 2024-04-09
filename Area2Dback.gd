8bce88409365235add88604d80728291b590d576
parthsetiya <132999404+parthsetiya@users.noreply.github.com>
2024-04-09 20:36:42 +1200

    added in house

---
+++ Area2Dback.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+
+var entered = false
+
+func _on_body_entered(body: PhysicsBody2D):
+	entered = true
+
+func _on_area_shape_exited():
+	entered = false
+	
+func _process(delta):
+	if entered == true:
+		if Input.is_action_just_pressed("Interact"):
+			get_tree().change_scene_to_file("res://scenes/maintest.tscn")
+
+
+
+
+
+
