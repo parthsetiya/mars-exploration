2e3380275a577ca8ed87f91fe5a32659b49c8a2f
Parth Setiya <parthsetiya@gmail.com>
2024-06-10 14:00:50 +1200

    gold animation works, doesnt go in inventory

--- scenes/gold_block.gd
+++ scenes/gold_block.gd
@@ -1,54 +1,54 @@
 extends Node2D
 
-var state = "no_gold" 
+var state = "no_gold"
 var player_in_area = false
-
-var gold = preload("res://scenes/gold_collectable.tscn")
+var gold_scene = preload("res://scenes/gold_collectable.tscn") as PackedScene
 
 @export var item: InvItem
 var player = null
 
+@onready var respawn_timer = $respawn_timer
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var marker = $Marker2D
+
 func _ready():
-	print(gold)
+	print(gold_scene)
 	if state == "no_gold":
-		$respawn_timer.start()
-	
-	
+		respawn_timer.start()
+
 func _process(delta):
 	if state == "no_gold":
-		$AnimatedSprite2D.play("no_gold")
-	if state == "gold":
-		$AnimatedSprite2D.play("gold")
-		if player_in_area == true:
-			print("yeeeeee")
-			if Input.is_action_just_pressed("Interact"):
-				state = "no_gold"
-				drop_gold()
+		animated_sprite.play("no_gold")
+	elif state == "gold":
+		animated_sprite.play("gold")
+		if player_in_area and Input.is_action_just_pressed("Interact"):
+			state = "no_gold"
+			drop_gold()
 
+func popfromground(gold_collectable):
+	gold_collectable.get_node("AnimatedSprite2D").show()
+	gold_collectable.get_node("AnimationPlayer2").play("poppingfromground")
+	await get_tree().create_timer(1.5).timeout
+	gold_collectable.get_node("AnimationPlayer2").play("fade")
+	await get_tree().create_timer(0.6).timeout
+	gold_collectable.queue_free()
 
 func _on_respawn_timer_timeout():
 	if state == "no_gold":
 		state = "gold"
 
 func drop_gold():
-	
-	var gold_instance = gold.instantiate()
-	gold_instance.global_position = $Marker2D.global_position
+	var gold_instance = gold_scene.instantiate()
+	gold_instance.global_position = marker.global_position / 2
 	get_parent().add_child(gold_instance)
+	popfromground(gold_instance)
 	player.collect(item)
 	await get_tree().create_timer(3).timeout
-	$respawn_timer.start()
-	
-
-
-
+	respawn_timer.start()
 
 func _on_area_2d_body_entered(body):
 	player_in_area = true
 	player = body
-	
-
-
 
 func _on_area_2d_body_exited(body):
 	player_in_area = false
