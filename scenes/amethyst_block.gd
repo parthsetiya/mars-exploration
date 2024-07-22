afa46c528ebc33140245e712a5ce9b1bfddac926
parthsetiya <132999404+parthsetiya@users.noreply.github.com>
2024-07-22 22:40:29 +1200

    added amethyst

--- scenes/amethyst_block.gd
+++ scenes/amethyst_block.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-var state = "no_gold"
+
+var state = "no_amethyst"
 var player_in_area = false
-var gold_scene = preload("res://scenes/gold_collectable.tscn") as PackedScene
+var amethyst_scene = preload("res://scenes/amethyst_collectable.tscn") as PackedScene
 var playerData = PlayerData.new()
 var player = null
 var collectable = false
@@ -15,50 +16,50 @@ var collectable = false
 @onready var marker = $Marker2D
 
 
-const item = preload("res://Inventory/items/gold.tres")
+const item = preload("res://Inventory/items/amethyst.tres")
 
 var player_node
 var inventory
-var current_gold
+var current_amethyst
 
 signal request_inventory_update()
 
 
 func _ready():
-	print(gold_scene)
-	if state == "no_gold":
+	print(amethyst_scene)
+	if state == "no_amethyst":
 		respawn_timer.start() 
 
 func _on_inventory_updated(new_inventory):
 	print("new inventory: " + str(new_inventory))
 
 func _process(delta):
-	if state == "no_gold":
-		animated_sprite.play("no_gold")
-	elif state == "gold":
-		animated_sprite.play("gold")
+	if state == "no_amethyst":
+		animated_sprite.play("no_amethyst")
+	elif state == "amethyst":
+		animated_sprite.play("amethyst")
 		if player_in_area and Input.is_action_just_pressed("Interact"):
-			state = "no_gold"
-			drop_gold()
+			state = "no_amethyst"
+			drop_amethyst()
 			add_item_to_inventory(item.name, 1)
 
-func popfromground(gold_collectable):
-	gold_collectable.get_node("AnimatedSprite2D").show()
-	gold_collectable.get_node("AnimationPlayer2").play("poppingfromground")
+func popfromground(amethyst_collectable):
+	amethyst_collectable.get_node("AnimatedSprite2D").show()
+	amethyst_collectable.get_node("AnimationPlayer").play("popfromgroundamethyst")
 	await get_tree().create_timer(1.5).timeout
-	gold_collectable.get_node("AnimationPlayer2").play("fade")
+	amethyst_collectable.get_node("AnimationPlayer").play("fadeamethyst")
 	await get_tree().create_timer(0.6).timeout
-	gold_collectable.queue_free()
+	amethyst_collectable.queue_free()
 
 func _on_respawn_timer_timeout():
-	if state == "no_gold":
-		state = "gold"
+	if state == "no_amethyst":
+		state = "amethyst"
 
-func drop_gold():
-	var gold_instance = gold_scene.instantiate()
-	gold_instance.global_position = marker.global_position / 2
-	get_parent().add_child(gold_instance)
-	popfromground(gold_instance)
+func drop_amethyst():
+	var amethyst_instance = amethyst_scene.instantiate()
+	amethyst_instance.global_position = marker.global_position / 2
+	get_parent().add_child(amethyst_instance)
+	popfromground(amethyst_instance)
 	await get_tree().create_timer(3).timeout
 	respawn_timer.start()
 	emit_signal("request_inventory_update", item.name, 1)
@@ -72,7 +73,6 @@ func _on_area_2d_body_exited(body):
 
 func add_item_to_inventory(item_name, quantity):
 	emit_signal("request_inventory_update", item_name, quantity)
-
 
 
 
---
+++ scenes/pickableareamethyst.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+
+var animation = preload("res://scenes/iron_collectable.tscn") as PackedScene
+@onready var amethyst_collectable = $amethyst_collectable
+@onready var amethyst_block = $".."
+
+
+var entered = false
+var collectable = false
+
+func _on_body_entered(body):
+	print($AnimationPlayer)
+	entered = true
+
+
+func _on_body_exited(body):
+	entered = false
