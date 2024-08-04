96dceac73b2fb605c78fca5eaf58683149ea3788
parthsetiya <132999404+parthsetiya@users.noreply.github.com>
2024-08-04 14:35:33 +1200

    pickaxe animation added

--- scripts/player.gd
+++ scripts/player.gd
@@ -18,115 +18,15 @@ var health = 100
 var save_file_path = "user://save/"
 var save_file_name = "PlayerSave.tres"
 var playerData = PlayerData.new()
-#var inventory = Inventory.new()
-#@onready var inventory_gui = $InventoryGui
+var input_direction
+var direction 
+@onready var toolanim = $Node2D/AnimationPlayer
+@onready var node_2d = $Node2D
 signal update_ui(health, position)
-#added reswawn location needs to be changed to last save location
 var respawn_position = Vector2(playerData.SavePos)
-
-# Node definitions
-#var gold_node
-#var inventory_slots
-#var inventory_gui_slots
-#var inventory_hotbar_slots
-#var first_slot_index = null
-#var second_slot_index = null
-#var inv_gui_show
-#var inv_gui_show_hotbar
 
 func _ready():
 	verify_save_directory(save_file_path)
-	#gold_node = get_node("gold_block")
-	#gold_node.connect("request_inventory_update", Callable(self, "_on_request_inventory_update"))
-	#inventory_gui.connect("slot_clicked", Callable(self, "_on_slot_clicked"))
-	#inventory_slots = get_node("InventoryGui/GridContainer").get_children()
-
-	#inventory_gui_slots = inventory_slots.slice(0, 15)
-	#inventory_hotbar_slots = inventory_slots.slice(20, 25)
-	#
-	#inv_gui_show = get_node("InventoryGui/NinePatchRect")
-	#inv_gui_show_hotbar = get_node("InventoryGui/NinePatchRect2")
-	
-	
-	
-#func _on_slot_clicked(slot_index):
-	#if first_slot_index == null:
-		#first_slot_index = slot_index
-	#else:
-		#second_slot_index = slot_index
-		#_swap_items(first_slot_index, second_slot_index)
-		#first_slot_index = null
-		#second_slot_index = null
-#
-#func _swap_items(slot_index_1, slot_index_2):
-	#var slot_1 = inventory_slots[slot_index_1]
-	#var slot_2 = inventory_slots[slot_index_2]
-#
-	#var item_1 = slot_1.get_children()[1].get_children()[0].get_children()[0]
-	#var label_1 = slot_1.get_children()[1].get_children()[0].get_children()[1]
-#
-	#var item_2 = slot_2.get_children()[1].get_children()[0].get_children()[0]
-	#var label_2 = slot_2.get_children()[1].get_children()[0].get_children()[1]
-#
-	#var temp_texture = item_1.texture
-	#var temp_text = label_1.text
-#
-	#item_1.texture = item_2.texture
-	#label_1.text = label_2.text
-#
-	#item_2.texture = temp_texture
-	#label_2.text = temp_text
-#
-	#print("Swapped items between slots " + str(slot_index_1) + " and " + str(slot_index_2))
-#
-#func _on_request_inventory_update(item_name, quantity):
-	#inventory.add_item(item_name, quantity)
-	#var updated_quantity = inventory.get_item_quantity(item_name)
-	#update_inventory_ui(item_name, updated_quantity)
-	#
-#
-#func update_inventory_ui(item_name, updated_quantity):
-	#
-	#for i in range(inventory_slots.size()):
-		#var slot = inventory_slots[i]
-		#if slot.get_child_count() != 0:
-			#var centre_container = slot.get_children()[1]
-			#var item = centre_container.get_children()[0].get_children()[0]
-			#var label = centre_container.get_children()[0].get_children()[1]
-#
-			#if item.texture == GOLD.texture:
-				#label.text = str(updated_quantity)
-				#return 
-#
-	#for i in range(inventory_slots.size()):
-		#var slot = inventory_slots[i]
-		#if slot.get_children()[1] != null:
-			#var centre_container = slot.get_children()[1]
-			#var item = centre_container.get_children()[0].get_children()[0]
-			#var label = centre_container.get_children()[0].get_children()[1]
-#
-			#
-			#if item.texture == null: 
-				#item.texture = GOLD.texture
-				#label.text = str(updated_quantity)
-				#return 
-				#
-	#for i in range(inventory_slots.size()):
-		#var slot = inventory_slots[i]
-		#if slot.get_children()[1] != null:
-			#var centre_container = slot.get_children()[1]
-			#var item = centre_container.get_children()[0].get_children()[0]
-			#var label = centre_container.get_children()[0].get_children()[1]
-#
-			#
-			#if item.texture == null: 
-				#item.texture = GOLD.texture
-				#label.text = str(updated_quantity)
-				#return 
-				#
-		#
-	
-	
 	
 func verify_save_directory(path: String):
 	DirAccess.make_dir_absolute(path)
@@ -140,11 +40,18 @@ func _process(delta):
 	#emit_signal("update_ui", playerData.health, self.position)
 	emit_signal("update_ui", playerData.health, self.position)
 	playerData.UpdatePos(self.position)
-	#if Input.is_action_just_pressed("Inventory"):
-		#inventoryopen()
+	if Input.is_action_just_pressed("right"):
+		direction = "right"
+
 	$Goldcounter.text = "Gold: %s" % playerData.invGoldIngot
-	
-	
+	if direction == "right" and Input.is_action_just_pressed("swing"):
+		node_2d.show()
+		toolanim.play("toolswingright")
+		await get_tree().create_timer(0.05).timeout
+		animations.play("axeswingright")
+		await get_tree().create_timer(0.6).timeout
+		node_2d.hide()
+		
 
 
 #checks players health
@@ -202,7 +109,7 @@ func _on_control_change_health(action):
 
 	
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
+	input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
 func updateAnimation():
