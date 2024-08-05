cf7dd713b2bad09c553a9d6cdfb693e9f7e26c73
Parth Setiya <parthsetiya@gmail.com>
2024-08-05 13:54:26 +1200

    slot highlighting works

--- scripts/main.gd
+++ scripts/main.gd
@@ -20,6 +20,7 @@ var inventory = Inventory.new()
 @onready var inventory_gui = $Player/Camera2D/InventoryGui
 var inv_open = false
 const SELECTEDSLOT = preload("res://art/mainart/selectedslot.tres")
+const DEFAULTSLOT = preload("res://art/mainart/defaultslot.tres")
 const GOLD = preload("res://Inventory/items/gold.tres")
 const IRON = preload("res://Inventory/items/iron.tres")
 const LOG = preload("res://Inventory/items/log.tres")
@@ -43,7 +44,6 @@ var ironcollectable = false
 var goldcollectable = false
 var treecollectable = false
 var goldcollectable2 = false
-var slotone
 var slotonetexture
 
 func _ready():
@@ -59,21 +59,13 @@ func _ready():
 	inventory_slots = get_node("Player/Camera2D/InventoryGui/GridContainer").get_children()
 
 	inventory_gui_slots = inventory_slots.slice(0, 15)
-	inventory_hotbar_slots = inventory_slots.slice(20, 25)
-	var slotone = inventory_hotbar_slots[0]  
+	inventory_hotbar_slots = inventory_slots.slice(20, 25) 
 	
-	if slotone.has_node("background"):  # Ensure the slot has a child named "background"
-		slotonetexture = slotone.get_node("background")  # Get the background node
-
-		if slotonetexture is Sprite2D:  # Ensure the background node is a Sprite2D
-			var background_texture = slotonetexture.texture  # Access the texture property
-
-			# Print to check if the correct texture is assigned
-			print(background_texture)
-		else:
-			print("Error: 'background' is not a Sprite2D")
-	else:
-		print("Error: No child named 'background'")
+	for slot in inventory_hotbar_slots:
+		if slot.has_node("background"):
+			var slot_background = slot.get_node("background")
+			if slot_background is Sprite2D:
+				slot_background.texture = DEFAULTSLOT.texture 
 	 
 	
 	inv_gui_show = get_node("Player/Camera2D/InventoryGui/NinePatchRect")
@@ -309,12 +301,25 @@ func _process(delta):
 		interact_to_read.hide()
 	if Input.is_action_just_pressed("Inventory"):
 		inventoryopen()
-	if Input.is_action_just_pressed("1"):  # Replace "ui_select" with your input action
-		if slotonetexture is Sprite2D:
-			slotonetexture.texture = SELECTEDSLOT.texture  # Assuming SELECTEDSLOT is defined and has a texture property
-		else:
-			print("Error: 'slotonetexture' is not a Sprite2D")
-		
+	for i in range(5):
+		var action_name = "ui_hotbar_" + str(i + 1)  # Define action names like ui_hotbar_1, ui_hotbar_2, etc.
+		if Input.is_action_just_pressed(action_name):
+			highlight_slot(i)
+
+func highlight_slot(index):
+	for slot in inventory_hotbar_slots:
+		if slot.has_node("background"):
+			var slot_background = slot.get_node("background")
+			if slot_background is Sprite2D:
+				slot_background.texture = DEFAULTSLOT.texture  # Set to default texture
+				
+	if index < inventory_hotbar_slots.size():
+		var selected_slot = inventory_hotbar_slots[index]
+		if selected_slot.has_node("background"):
+			var selected_slot_background = selected_slot.get_node("background")
+			if selected_slot_background is Sprite2D:
+				selected_slot_background.texture = SELECTEDSLOT.texture
+	
 	
 func pausemenu():
 	if paused:
