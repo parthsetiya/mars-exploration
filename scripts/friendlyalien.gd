eaaed3aef5b38eff8a870f37b7eeb91b1097309e
Parth Setiya <parthsetiya@gmail.com>
2024-08-15 14:10:20 +1200

    DIALOGUE IMPLEMENTED

--- scripts/friendlyalien.gd
+++ scripts/friendlyalien.gd
@@ -3,13 +3,19 @@ extends CharacterBody2D
 var in_talkable = false
 var player = null
 var playerData = PlayerData.new()
+@onready var example_balloon = $ExampleBalloon
 @onready var dialogue = $dialogue
 signal request_inventory_update()
 const REMOVE_GOLD = preload("res://Inventory/items/remove_gold.tres")
+var resource = load("res://dialogue/testing.dialogue")
+var title = load("res://dialogue/testing.dialogue")
+#var dialogue_line = await resource.get_next_dialogue_line("start")
+
 func _on_talkablearea_body_entered(body):
 	in_talkable = true
 	player = body
-	
+	DialogueManager.show_example_dialogue_balloon(resource, "start")
+
 
 func _on_talkablearea_body_exited(body):
 	in_talkable = false
@@ -17,8 +23,7 @@ func _on_talkablearea_body_exited(body):
 	
 func _process(delta):
 	if in_talkable == true:
-		dialogue.show()
-	
+		
 		if Input.is_action_just_pressed("Interact"):
 			playerData.load_data() 
 			if playerData.invGoldIngot >=2:
