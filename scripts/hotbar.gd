65b2d26b331bc5b5cb41b3a4bc12357d47d8f056
Itsparrotdude <noah.volante@gmail.com>
2024-08-19 14:08:46 +1200

    goatbar

--- scripts/hotbar.gd
+++ scripts/hotbar.gd
@@ -1,32 +1,38 @@
 extends Control
 
 var inventory: Node
-@onready var grid_container = $GridContainer
+@onready var h_box_container = $HBoxContainer
+
+var item_images = {
+	"Pebble": preload("res://art/mainart/pebble.png")
+	#"gold": preload("res://images/gold.png"),
+	#"wood": preload("res://images/wood.png"),
+	#"amethyst": preload("res://images/amethyst.png"),
+	#"Golden joint": preload("res://images/golden_joint.png")
+}
 
 func _process(delta):
 	inventory = get_node("/root/Inventory")
 	update_hotbar()
 
 func update_hotbar():
-	var hotbar_items = ["Gold", "Pebble", "Wood"]  # Define the items to show in the hotbar
+	var hotbar_items = ["Pebble", "item2", "item3"]  # Define the items to show in the hotbar
+	
 	for i in range(hotbar_items.size()):
 		var item_name = hotbar_items[i]
-		var item_button = $HBoxContainer.get_child(i) as Button
+		var item_button = h_box_container.get_child(i) as Button
 		if inventory.items.has(item_name):
-			item_button.text = item_name
+			item_button.text = str(inventory.items[item_name])  # Show the quantity
 		else:
-			item_button.text = ""
+			item_button.text = "0"
 
-#var selected_item: String = ""
-#
-#func _on_item_button_pressed(button):
-	#selected_item = button.text
-	#trigger_animation_for_selected_item()
-#
-#func trigger_animation_for_selected_item():
-	#if selected_item == "item1":
-		## Trigger specific animation for item1
-		#$Character.play("animation1")
-	#elif selected_item == "item2":
-		## Trigger specific animation for item2
-	#$Character.play("animation2")
+	# Update textures in the hotbar
+	for i in range(hotbar_items.size()):
+		var item_name = hotbar_items[i]
+		if inventory.items.has(item_name) and item_images.has(item_name):
+			var texture_rect = TextureRect.new()
+			texture_rect.texture = item_images[item_name]
+			texture_rect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED  # Ensures the texture fits well
+			var container = h_box_container.get_child(i) as Button
+			if container:
+				container.add_child(texture_rect)
