b7ccfb3cdc3c440b662797cc01bb0dfe7b1e2d70
Parth Setiya <parthsetiya@gmail.com>
2024-10-16 10:19:10 +1300

    made shop better

--- blacksmithshop.gd
+++ blacksmithshop.gd
@@ -7,15 +7,8 @@ const GOLD_PICKAXE = preload("res://Inventory/items/gold_pickaxe.tres")
 func add_item_to_inventory(item_name, quantity):
 	emit_signal("request_inventory_update", item_name, quantity)
 
-func _on_texture_button_pressed():
+
+func _on_button_pressed():
 	playerdata.load_data()
 	if playerdata.invpickaxehead >= 1 && playerdata.invstick >=1:
 		add_item_to_inventory(GOLD_PICKAXE.name, 1)
-
-
-func _on_texture_button_2_pressed():
-	pass
-
-
-func _on_texture_button_3_pressed():
-	pass 
